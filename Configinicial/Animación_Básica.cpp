23616740cfc39419e01a8dff120363edbea3059b
Diego Del Razo <158770774+sorryihateyou@users.noreply.github.com>
2025-04-14 13:38:03 -0600

    Corrección de errores external libraries

--- Configinicial/Animación_Básica.cpp
+++ Configinicial/Animación_Básica.cpp
@@ -245,7 +245,7 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Proyecto Laboratorio Computación Gráfica", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Proyecto Laboratorio Computacion Grafica", nullptr, nullptr);
 
 	if (nullptr == window)
 	{
